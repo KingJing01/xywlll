6e4f9068e2f0c08d21db5c410e5f58382c36469f
youjing.li <li.youjing@cesgroup.com.cn>
2018-07-19 13:15:43 +0800

    修改项目启动的默认界面

--- src/main/java/com/xinya/coldchain/xinya/DefaultView.java
+++ src/main/java/com/xinya/coldchain/xinya/DefaultView.java
@@ -1,18 +1,17 @@
 package com.xinya.coldchain.xinya;
 
-import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 /**
- * 设置默认的跳转界面.
+ * 自定义跳转界面.
  */
 public class DefaultView extends WebMvcConfigurationSupport {
 
+
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("forward:/index");
-        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        registry.addViewController("/").setViewName("index");
         super.addViewControllers(registry);
     }
 }
