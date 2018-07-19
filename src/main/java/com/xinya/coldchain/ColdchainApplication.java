94305ae6351b644f2c115ec046a6cfcc0f6ee352
youjing.li <li.youjing@cesgroup.com.cn>
2018-07-19 11:35:03 +0800

    修改项目启动的默认界面

---
+++ src/main/java/com/xinya/coldchain/ColdchainApplication.java
@@ -0,0 +1,15 @@
+package com.xinya.coldchain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * SpringBoot启动界面.
+ */
+@SpringBootApplication
+public class ColdchainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ColdchainApplication.class, args);
+    }
+}
