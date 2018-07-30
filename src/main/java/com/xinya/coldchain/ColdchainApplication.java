9c983b185af1dc6a3a4949b94293e8f69bfc6aa6
youjing.li <li.youjing@cesgroup.com.cn>
2018-07-30 11:02:07 +0800

    file update add ekan

--- src/main/java/com/xinya/coldchain/ColdchainApplication.java
+++ src/main/java/com/xinya/coldchain/ColdchainApplication.java
@@ -2,11 +2,13 @@ package com.xinya.coldchain;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 /**
  * SpringBoot启动界面.
  */
 @SpringBootApplication
+@EnableCaching
 public class ColdchainApplication {
 
     public static void main(String[] args) {
