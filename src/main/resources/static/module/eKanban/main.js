b607f77b5437850ca77763831de529684826207e
youjing.li <li.youjing@cesgroup.com.cn>
2018-08-02 14:06:00 +0800

    echarts update

--- src/main/resources/static/module/eKanban/main.js
+++ src/main/resources/static/module/eKanban/main.js
@@ -1,6 +1,6 @@
 (function () {
     // HTML 界面
-    var PAGE_HTML="module/eKanban/main.html";
+    var PAGE_HTML = "module/eKanban/main.html";
     define(
         [
             'jquery',
@@ -8,55 +8,61 @@
             'text!' + PAGE_HTML
         ],
         function
-            ($,echarts, pageHtml) {
+            ($, echarts, pageHtml) {
             var initHtml = function () {
                 $("#page-wrapper").html(pageHtml);
             };
             /*异常分布类型  饼图*/
             var initPie = function () {
                 var myChart = echarts.init(document.getElementById('right_chart'));
-                var option = {
-                    title: {
-                        text: '异常类型分布',
-                        x: 'left'
-                    },
-                    tooltip: {
-                        trigger: 'item',
-                        formatter: "{a} <br/>{b} : {c} ({d}%)"
-                    },
-                    legend: {
-                        orient: 'horizontal',
-                        bottom: '30',
-                        data: ['直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎']
-                    },
-                    series: [
-                        {
-                            name: '访问来源',
-                            type: 'pie',
-                            radius: ['50%', '70%'],
-                            center: ['50%', '40%'],
-                            data: [
-                                {value: 335, name: '直接访问'},
-                                {value: 310, name: '邮件营销'},
-                                {value: 234, name: '联盟广告'},
-                                {value: 135, name: '视频广告'},
-                                {value: 1548, name: '搜索引擎'}
-                            ],
-                            itemStyle: {
-                                emphasis: {
-                                    shadowBlur: 10,
-                                    shadowOffsetX: 0,
-                                    shadowColor: 'rgba(0, 0, 0, 0.5)'
+                myChart.showLoading();
+                $.get('').done(function (data) {
+                    myChart.hideLoading();
+                    myChart.setOption({
+                        title: {
+                            text: '异常类型分布',
+                            x: 'left'
+                        },
+                        tooltip: {
+                            trigger: 'item',
+                            formatter: "{a} <br/>{b} : {c} ({d}%)"
+                        },
+                        legend: {
+                            orient: 'horizontal',
+                            bottom: '30',
+                            data: ['直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎']
+                        },
+                        series: [
+                            {
+                                name: '访问来源',
+                                type: 'pie',
+                                radius: ['50%', '70%'],
+                                center: ['50%', '40%'],
+                                data: [
+                                    {value: 335, name: '直接访问'},
+                                    {value: 310, name: '邮件营销'},
+                                    {value: 234, name: '联盟广告'},
+                                    {value: 135, name: '视频广告'},
+                                    {value: 1548, name: '搜索引擎'}
+                                ],
+                                itemStyle: {
+                                    emphasis: {
+                                        shadowBlur: 10,
+                                        shadowOffsetX: 0,
+                                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+                                    }
                                 }
                             }
-                        }
-                    ]
-                };
-                myChart.setOption(option);
+                        ]
+                    });
+                });
             };
             var initPickUpRate = function () {
-                    var myChart = echarts.init(document.getElementById('pick_up_rate'));
-                    var option = {
+                var myChart = echarts.init(document.getElementById('pick_up_rate'));
+                myChart.showLoading();
+                $.get('').done(function (data) {
+                    myChart.hideLoading();
+                    myChart.setOption({
                         title: {
                             text: '提货准点率',
                             x: 'left'
@@ -73,12 +79,15 @@
                             type: 'line',
                             smooth: true
                         }]
-                    };
-                    myChart.setOption(option);
-                };
-               var initArrivalRate = function () {
-                    var myChart = echarts.init(document.getElementById('arrival_rate'));
-                    var option = {
+                    });
+                });
+            };
+            var initArrivalRate = function () {
+                var myChart = echarts.init(document.getElementById('arrival_rate'));
+                myChart.showLoading();
+                $.get('').done(function (data) {
+                    myChart.hideLoading();
+                    myChart.setOption({
                         title: {
                             text: '到货准点率',
                             x: 'left'
@@ -96,10 +105,23 @@
                             type: 'line',
                             areaStyle: {}
                         }]
-                    };
-                    myChart.setOption(option);
-                }
-
+                    });
+                });
+            }
+            var bindEvents = function () {
+                /* 提货率 */
+                $("#pick_up_rate_container li").each(function (index) {
+                    $(this).click(function () {
+                        initPickUpRate(index);
+                    })
+                })
+                /* 到货率  */
+                $("#arrival_rate_container li").each(function (index) {
+                    $(this).click(function () {
+                        initArrivalRate(index);
+                    })
+                })
+            }
 
             var ekanMain = {};
             ekanMain.load = function () {
@@ -107,6 +129,7 @@
                 initPie();
                 initPickUpRate();
                 initArrivalRate();
+                bindEvents();
             }
             return ekanMain;
         });
