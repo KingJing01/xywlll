d03b6a630f6d389bb371ee4d251faebc25ab15b8
liyoujing <18049808631@163.com>
2018-08-16 12:04:54 +0800

    tms reource update

---
+++ src/main/java/com/xinya/coldchain/mapper/TmsUserMapper.java
@@ -0,0 +1,24 @@
+package com.xinya.coldchain.mapper;
+
+import com.xinya.coldchain.sys.model.TmsUser;
+
+
+/**
+ * @author liyoujing
+ * @create 2018-08-06 下午 04:19
+ * @desc 系统通用模块Dao
+ **/
+
+public interface TmsUserMapper {
+
+     /**
+      * 用户名获取用户信息
+      * @param username 登陆用户名
+      * @return 返回tms数据
+      */
+ /*    @Select("select pk_user pkUser,user_name userName,user_code userCode,user_type user_type,"
+             + "user_password userPassword from nw_user where dr=0 and  user_code=#{username}")*/
+     TmsUser getUserInfoByUsername(String username);
+
+     TmsUser selectUserByPk(String pk);
+}
