956f0a55fa43115c5ecea1a02b8896de78aa1b90
liyoujing <18049808631@163.com>
2018-08-16 16:00:08 +0800

    tms reource update

--- src/main/resources/static/module/userManage/cargoOwner/cargoOwner.js
+++ src/main/resources/static/module/userManage/cargoOwner/cargoOwner.js
@@ -1,18 +1,20 @@
 /* 货主审核模块的 js  */
 (function () {
-    var HTML_PAGE="module/userManage/cargoOwner/cargoOwner.html";
+    var HTML_PAGE = "module/userManage/cargoOwner/cargoOwner.html";
     define([
         'jquery',
-        'text!'+ HTML_PAGE
-    ],function ($,pageHtml) {
-       var initHtml = function () {
-           $("#page-wrapper").html(pageHtml);
-       }
-     var hostObj ={};
-     hostObj.load(function () {
-         initHtml();
-     })
+        'text!' + HTML_PAGE
+    ], function ($, pageHtml) {
+        var initHtml = function () {
+            $("#page-wrapper").html(pageHtml);
+        }
 
+
+        var hostObject = {};
+        hostObject.load(function () {
+            initHtml();
+        })
+        return hostObject;
     })
 
 }).call(this)
--- src/main/resources/static/module/userManage/driver/driver.js
+++ src/main/resources/static/module/userManage/driver/driver.js
@@ -1,15 +1,20 @@
 /* 司机模块审核的 js  */
 (function () {
-    var HTML_PAGE="module/userManage/driver/driver.html";
+    var HTML_PAGE = "module/userManage/driver/driver.html";
     define([
         'jquery',
-         'text!'+ HTML_PAGE
-    ],function ($,html_page) {
+        'text!' + HTML_PAGE
+    ], function ($, pageHtml) {
+        var initHtml = function () {
+            $("#page-wrapper").html(pageHtml);
+        }
 
 
-
-
-
+        var driverObject = {};
+        driverObject.load(function () {
+            initHtml();
+        })
+        return driverObject;
 
     })
 
