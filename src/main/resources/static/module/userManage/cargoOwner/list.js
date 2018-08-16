6f18ce219fa83fbadda571a4c9ba4df1a9252f70
liyoujing <18049808631@163.com>
2018-08-16 16:46:15 +0800

    tms reource update

--- src/main/resources/static/module/userManage/cargoOwner/list.js
+++ src/main/resources/static/module/userManage/cargoOwner/list.js
@@ -5,14 +5,33 @@
         'jquery',
         'text!' + HTML_PAGE
     ], function ($, pageHtml) {
+        //初始化界面
         var initHtml = function () {
             $("#page-wrapper").html(pageHtml);
+        }
+
+        //初始化bootstrap table
+        var initTable = function () {
+            $('#cargo_owner_table').bootstrapTable({
+                data: ,
+                columns: [{
+                    field: 'id',
+                    title: 'Item ID'
+                }, {
+                    field: 'name',
+                    title: 'Item Name'
+                }, {
+                    field: 'price',
+                    title: 'Item Price'
+                }, ]
+            });
         }
 
 
         var hostObject = {};
         hostObject.load = function () {
             initHtml();
+            initTable();
         }
         return hostObject;
     })
