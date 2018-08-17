23bd4749be5433492ad5397c3c929a4b9475be68
liyoujing <18049808631@163.com>
2018-08-17 17:30:10 +0800

    tms reource update

--- src/main/resources/static/module/userManage/cargoOwner/list.js
+++ src/main/resources/static/module/userManage/cargoOwner/list.js
@@ -2,6 +2,7 @@
 (function () {
     var HTML_PAGE = "module/userManage/cargoOwner/list.html";
     var DEMO_DATA = "data/data1.json";
+    var LIST_DATA = "cargo_owner/get_list_data";
     define([
         'jquery',
         'text!' + HTML_PAGE,
@@ -15,8 +16,8 @@
         //初始化bootstrap table
         var initTable = function () {
             $('#cargo_owner_table').bootstrapTable({
-                url: "cargo_owner/get_list_data",
-                contentType:'application/x-www-form-urlencoded',
+                url:LIST_DATA,
+                contentType: 'application/x-www-form-urlencoded',
                 method: 'post',
                 pageList: [10, 15, 20],
                 pagination: true,
@@ -28,7 +29,7 @@
                     var param = {
                         pageSize: this.pageSize,   //每页多少条数据
                         pageNumber: this.pageNumber, // 页码
-                        custCode:''
+                        custCode: null
                     };
                     return param;
                 },
@@ -41,56 +42,52 @@
                 columns: [{
                     field: 'rowNum',
                     title: '序号',
-                    align : 'center',
-                    valign : 'middle',
-                    //width:'10%'
+                    align: 'center',
+                    valign: 'middle'
                 }, {
                     field: 'custName',
                     title: '昵称',
-                    align : 'center',
-                    //width:'15%'
+                    align: 'center'
                 }, {
                     field: 'custCode',
                     title: '注册账号',
-                    align : 'center',
-                    //width:'20%'
+                    align: 'center'
                 }, {
                     field: 'checkStatus',
                     title: '是否认证',
-                    align : 'center',
-                    //width:'10%',
-                    formatter:function(value){
-                     return value==2?'是':'否';
+                    align: 'center',
+                    formatter: function (value) {
+                        return value == 2 ? '是' : '否';
                     }
 
                 }, {
                     field: 'createTime',
                     title: '注册时间',
-                    align : 'center',
-                    //width:'20%'
+                    align: 'center',
                 }, {
                     field: 'checkStatus',
                     title: '状态',
-                    align : 'center',
-                    formatter:function (value) {
-                      return value==4?'冻结':'正常';
-                    },
-                    //width:'15%'
-
-                },{
+                    align: 'center',
+                    formatter: function (value) {
+                        return value == 4 ? '冻结' : '正常';
+                    }
+                }, {
                     field: 'pkCustomer',
                     title: '操作',
-                    align : 'center',
-                    formatter:function (value) {
-                        return "<div id='"+value+"'><a href='#' class='detail'>查看</a><a href='#' class='freeze'>冻结</a><a href='#' class='del'>删除</a></div>";
-                    },
-                   //width:'10%'
+                    align: 'center',
+                    formatter: function (value) {
+                        return "<div id='" + value + "'><a href='#' class='detail'>查看</a><a href='#' class='freeze audit_a'>冻结</a><a href='#' class='del audit_a'>删除</a></div>";
+                    }
                 }]
             });
         }
         //点击事件的绑定
         var bindEvent = function () {
-
+            $("#cargo_search").click(function () {
+                $("#cargo_owner_table").bootstrapTable('refresh', {
+                    query: {custCode: $("#cargo_text").val()}
+                });
+            })
         }
 
         var hostObject = {};
