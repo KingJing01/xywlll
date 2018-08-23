d8c82441912a4b941ee48605e89b1712d1d278fc
liyoujing <18049808631@163.com>
2018-08-23 21:47:27 +0800

    tms reource update

--- src/main/resources/static/module/userManage/fleet/detail.js
+++ src/main/resources/static/module/userManage/fleet/detail.js
@@ -99,7 +99,7 @@
                                 data.id_card_pos = (data.id_card_pos ? window.imgUrl + data.id_card_pos : common.noImage);
                                 _self.resp = data;
                             } else {
-                                data.id_card_post = (data.id_card_post ? window.imgUrl + data.id_card_post : common.noImage);
+                                data.id_card_pos = (data.id_card_pos ? window.imgUrl + data.id_card_pos : common.noImage);
                                 data.business_license = (data.business_license ? window.imgUrl + data.business_license : common.noImage);
                                 data.road_trans_license = (data.road_trans_license ? window.imgUrl + data.road_trans_license : common.noImage);
                                 _self.corp = data;
