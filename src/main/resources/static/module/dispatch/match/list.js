878cf7cab5777a99dd2399663697a0e06fdeb4d3
liyoujing <18049808631@163.com>
2018-09-11 14:26:17 +0800

    新增城市拉下选择

--- src/main/resources/static/module/dispatch/match/list.js
+++ src/main/resources/static/module/dispatch/match/list.js
@@ -4,11 +4,30 @@
     var JSON_DATA = "tms_system/public/httpEdi/Sto/loadData.do";
     define([
             'jquery',
+            'vue',
             'text!' + HTML_LIST
-        ], function ($, htmlList) {
+        ], function ($, Vue, htmlList) {
+            var totalData;
             /*指定位置加载html界面*/
             var initDetailHtml = function () {
                 $("#page-wrapper").html(htmlList);
+            }
+            var changeTotalData = function (num, weight, volume) {
+                totalData._data.num = num;
+                totalData._data.weight = weight;
+                totalData._data.volume = volume;
+            }
+
+            /*利用vue进行界面内容的渲染*/
+            var initVueData = function () {
+                totalData = new Vue({
+                    el: '#dispatch_match_total',
+                    data: {
+                        num: 0,
+                        weight: 0,
+                        volume: 0
+                    }
+                });
             }
 
             var initComponent = function () {
@@ -18,6 +37,34 @@
                 $("#arrival_city").click(function (e) {
                     SelCity(this, e);
                 });
+            }
+            /*增加统计的数据*/
+            var addTotalData = function (data) {
+                length = data.length;
+                var num = length > 1 ? 0 : totalData._data.num;
+                var weight = length > 1 ? 0 : totalData._data.weight;
+                var volumn = length > 1 ? 0 : totalData._data.volume;
+                for (var j = 0; j < length; j++) {
+                    var arr = data[j].num_weight_volume.split(",");
+                    num = num + Number(arr[0]);
+                    weight = weight + Number(arr[1]);
+                    volumn = volumn + Number(arr[2]);
+                }
+                changeTotalData(num, weight, volumn);
+            }
+            /*减少统计的数据*/
+            var reduceTotalData = function (data) {
+                length = data.length;
+                var num = length > 1 ? 0 : totalData._data.num;
+                var weight = length > 1 ? 0 : totalData._data.weight;
+                var volumn = length > 1 ? 0 : totalData._data.volume;
+                for (var  k= 0; k < length; k++) {
+                    var arr = data[k].num_weight_volume.split(",");
+                    num = num - Number(arr[0]);
+                    weight = weight - Number(arr[1]);
+                    volumn = volumn - Number(arr[2]);
+                }
+                changeTotalData(num, weight, volumn);
             }
             var initTable = function () {
                 $('#dispatch_match_table').bootstrapTable({
@@ -50,9 +97,9 @@
                         field: 'num',
                         title: '序号',
                         align: 'center',
-                        formatter:function(value,row,index){
-                            var pageSize=$('#dispatch_match_table').bootstrapTable('getOptions').pageSize
-                            var pageNumber=$('#dispatch_match_table').bootstrapTable('getOptions').pageNumber;
+                        formatter: function (value, row, index) {
+                            var pageSize = $('#dispatch_match_table').bootstrapTable('getOptions').pageSize
+                            var pageNumber = $('#dispatch_match_table').bootstrapTable('getOptions').pageNumber;
                             return pageSize * (pageNumber - 1) + index + 1;
                         }
                     }, {
@@ -74,7 +121,7 @@
                         formatter: function (value) {
                             var arr = new Array();
                             arr = value.split(",");
-                            return arr[0]+"件/"+arr[1]*1+"吨/"+arr[2]*1+"立方";
+                            return arr[0] + "件/" + arr[1] * 1 + "吨/" + arr[2] * 1 + "立方";
                         }
                     }, {
                         field: 'pk_trans_type',
@@ -85,14 +132,14 @@
                         title: '提货',
                         align: 'center',
                         formatter: function (value) {
-                            return  value.replace(","," \n ");
+                            return value.replace(",", " \n ");
                         }
                     }, {
                         field: 'arri',
                         title: '到货',
                         align: 'center',
                         formatter: function (value) {
-                            return  value.replace(","," \n ");
+                            return value.replace(",", " \n ");
                         }
                     }, {
                         field: 'pkCustomer',
@@ -102,7 +149,23 @@
                             var str = "<div id='" + value + "'><a href='#' class='detail'>查看</a><a href='#' class='thaw audit_a'>放弃订单</a>";
                             return str + "</div>";
                         }
-                    }]
+                    }],
+                    onCheck: function (row) {
+                        var arr = new Array();
+                        arr[0] = row;
+                        addTotalData(arr);
+                    },
+                    onUncheck: function (row) {
+                        var arr = new Array();
+                        arr[0] = row;
+                        reduceTotalData(arr)
+                    },
+                    onCheckAll: function (rows) {
+                        addTotalData(rows);
+                    },
+                    onUncheckAll: function () {
+                        changeTotalData(0, 0, 0)
+                    }
                 });
             }
             /* 事件绑定操作 */
@@ -115,6 +178,7 @@
                 initDetailHtml();
                 initComponent();
                 initTable();
+                initVueData();
                 bindEvent();
             }
             return object;
