b80731969687ded421679ddb78130ad6fc556f73
liyoujing <18049808631@163.com>
2018-09-17 18:47:28 +0800

    add jquery_raty

--- src/main/java/com/xinya/coldchain/tools/shiro/filter/ShiroPermissionsFilter.java
+++ src/main/java/com/xinya/coldchain/tools/shiro/filter/ShiroPermissionsFilter.java
@@ -1,17 +1,9 @@
 package com.xinya.coldchain.tools.shiro.filter;
 
-import com.google.gson.Gson;
 import org.apache.shiro.web.filter.authz.PermissionsAuthorizationFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 public class ShiroPermissionsFilter extends PermissionsAuthorizationFilter {
 
